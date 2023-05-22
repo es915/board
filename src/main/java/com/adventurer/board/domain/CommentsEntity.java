3c18992a79f28e0341a995c23de342eccdae7161
baebin <spaiderkids@naver.com>
2023-05-22 17:52:47 +0900

    댓글 entity작성

---
+++ src/main/java/com/adventurer/board/domain/CommentsEntity.java
@@ -0,0 +1,36 @@
+package com.adventurer.board.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Table (name ="Comments")
+@Entity
+public class CommentsEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content; // 댓글 내용
+
+    @Column()
+    private String createDate;
+
+    @ManyToOne
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user; // 작성자
+}
+
+}
