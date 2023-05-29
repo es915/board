779c50c0b3c7e12184a580317a0ddfbabec8a3fb
eulseon <eulseon2004@gmail.com>
2023-05-29 14:25:59 +0900

    댓글 레포지토리

---
+++ src/main/java/com/adventurer/board/repository/CommentRepository.java
@@ -0,0 +1,7 @@
+package com.adventurer.board.repository;
+
+import com.adventurer.board.domain.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
