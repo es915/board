6429b7dc3c0ec7b35fa791333a9dfcb7bfe4de3f
baebin <spaiderkids@naver.com>
2023-05-29 17:01:24 +0900

    c

--- src/main/java/com/adventurer/board/service/CommentService.java
+++ src/main/java/com/adventurer/board/service/CommentService.java
@@ -4,13 +4,16 @@ import com.adventurer.board.domain.Comment;
 
 import java.util.List;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public interface CommentService {
 
-    void save(Comment comment);
+    static void save(Comment comment) {
+        // 구현 로직
+    }
 
     Comment findById(Long id) throws Exception;
 
-
     List<Comment> findAll();
-
 }
